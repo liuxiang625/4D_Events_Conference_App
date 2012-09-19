61d85ff78decc7a05cf5e5fde0dc759aca2c4307
Xiang Liu <liuxiang625@gmail.com>
2012-09-19 10:22:05 -0700

    carousel and banner added, will pick one of them

--- SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
+++ SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
@@ -2,10 +2,27 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+//		    (function($) {
+//      //$("#carousel1").carousel();
+//      //$("#carousel2").carousel({direction: "vertical"});
+//  		})(jQuery);
+//  		$('#right').cycle({
+//     	 	fx: 'scrollRight',
+//      		next: '#right',
+//      		timeout: 0,
+//      		easing: 'easeInOutBack'
+//  		});
+			$('#right').cycle()
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
