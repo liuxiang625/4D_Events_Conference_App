b09b86ab23b054d617fa87925123a72a4cd4b7f5
Xiang Liu <liuxiang625@gmail.com>
2012-09-26 10:43:21 -0700

    Feedbacks received. Starting updating before implementation

--- SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
+++ SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
@@ -9,7 +9,40 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-
+//		$(".loadDays").click(function(){ 
+//			  console.log(this.id);
+//			  WAF.ds.Event.query("name = :1 | name %% :1", this.id, {
+//	                autoExpand: "sessions", 
+//	                onSuccess: function(event) { 
+//	                	//console.log(event.entityCollection.length);
+//	                	event.entityCollection.forEach({
+//	                        onSuccess: function(ev) {
+//	                        	var startDate = ev.entity.startDate.getValue();
+//	                        	var endDate = ev.entity.endDate.getValue();
+//    							$('#daysListView').empty().listview('refresh');
+//	                        	$('#daysListView').append('<li data-theme="c">' + '<a class="loadTimes" href="#page3" data-transition="slide">'+ '<h3>'+ formatDate(startDate) +'</h3>' + '</li>').listview('refresh');
+//	                        	while(startDate < endDate){
+//	                        		startDate.setDate(startDate.getDate() + 1);
+//		                        	console.log(Math.round((ev.entity.endDate.getValue() - ev.entity.startDate.getValue()) / (1000*60*60*24)));
+//		                        	$('#daysListView').append('<li data-theme="c">' + '<a class="loadTimes" href="#page3" data-transition="slide">'+ '<h3>'+ formatDate(startDate) +'</h3>' + '</li>').listview('refresh');
+// 
+//								}
+//	                        }
+//	                    })
+//	          		}
+//	          })
+//		});
+//		function formatDate(date) {// ultility to formate date to mm/dd/yyyy
+//			return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
+//		};
+//		$(".loadTimes").click(function(){ 
+//			  console.log(this.id);
+//			  WAF.ds.Event.query("fullName = :1", WAF.directory.currentUser().fullName, {
+//	                autoExpand: "pTO_RequestCollection",
+//	                onSuccess: function(event) {
+//	          		}
+//	          })
+//		});
 
 //		    (function($) {
 //      $("#carousel1").carousel();
