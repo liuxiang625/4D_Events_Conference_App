da7bace07f27adef70473b8884f1749834b06ae4
Xiang Liu <liuxiang625@gmail.com>
2012-09-26 18:16:46 -0700

    days generation updated(get everything in one shot)

--- SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
+++ SummitApp/SummitApp Solution/summitApp/WebFolder/smartphone/scripts/summitApp.js
@@ -9,6 +9,53 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
+		var eventCollections;
+		ds.Event.all({
+			orderBy:"startDate",
+			autoExpand: "sessions",
+			onSuccess:function(event) {	
+				var count = event.entityCollection.length;
+//				eventCollections = event.entityCollection;
+//				console.log(eventCollections.length);
+				$(".loadDays").click(function(){ 
+			    	console.log(this.id);
+			    	daysPageGeneration(event.entityCollection,this.id);
+			    });
+//				event.entityCollection.forEach({
+//				onSuccess: function(event2) {
+//					//myHTML += '<p>' + escapeHTML(event2.entity.name.getValue()) + '</p>';
+//				},
+//				atTheEnd: function(end) {
+//					var stop = new Date();
+//					var executionTime = stop - start;
+//					$('#container2').html(myHTML);
+//					$$('richText9').setValue(executionTime+ "ms");	
+//				}
+//			});		
+		}}); 
+		console.log(eventCollections);
+		function daysPageGeneration(eventCollections,day){
+			eventCollections.query("name = :1 | name %% :1", day, {
+	                autoExpand: "sessions", 
+	                onSuccess: function(event) { 
+	                	//console.log(event.entityCollection.length);
+	                	event.entityCollection.forEach({
+	                        onSuccess: function(ev) {
+	                        	var startDate = ev.entity.startDate.getValue();
+	                        	var endDate = ev.entity.endDate.getValue();
+    							$('#daysListView').empty().listview('refresh');
+	                        	$('#daysListView').append('<li data-theme="c">' + '<a class="loadTimes" href="#page3" data-transition="slide">'+ '<h3>'+ getTheDay(startDate) + " " + formatDate(startDate) +'</h3><p>4D Summit Pre-Class</p>' + '</li>').listview('refresh');
+	                        	while(startDate < endDate){
+	                        		startDate.setDate(startDate.getDate() + 1);
+		                        	//console.log(Math.round((ev.entity.endDate.getValue() - ev.entity.startDate.getValue()) / (1000*60*60*24)));
+		                        	$('#daysListView').append('<li data-theme="c">' + '<a class="loadTimes" href="#page3" data-transition="slide">'+ '<h3>'+ getTheDay(startDate) + " " + formatDate(startDate) +'</h3><p>4D Summit keynotes  breakout sessions</p>' + '</li>').listview('refresh');
+ 
+								}
+	                        }
+	                    })
+	          		}
+	          })
+		}
 //		$(".loadDays").click(function(){ 
 //			  console.log(this.id);
 //			  WAF.ds.Event.query("name = :1 | name %% :1", this.id, {
@@ -32,9 +79,24 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 //	          		}
 //	          })
 //		});
-//		function formatDate(date) {// ultility to formate date to mm/dd/yyyy
-//			return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
-//		};
+		$('.goBack').click(function(){
+			if($('#daysListView li').size() > 1){	
+				$.mobile.changePage("#page1", { transition: "slide",reverse:true});
+			}
+			else {
+				$.mobile.changePage("#page0", { transition: "slide",reverse:true});
+			}
+				
+		})
+		function formatDate(date) {// ultility to formate date to mm/dd/yyyy
+			return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
+		};
+		function getTheDay(date)
+		{
+
+			weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+			return weekday[date.getDay()];
+		}
 //		$(".loadTimes").click(function(){ 
 //			  console.log(this.id);
 //			  WAF.ds.Event.query("fullName = :1", WAF.directory.currentUser().fullName, {
