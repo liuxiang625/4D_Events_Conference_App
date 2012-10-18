544a524929224bbc3294671e8ee8ff6230b99bee
Xiang Liu <liuxiang625@gmail.com>
2012-10-18 14:31:00 -0700

    Server side survey answer creat & save implemented

--- SummitApp/SummitApp Solution/summitApp/Model.js
+++ SummitApp/SummitApp Solution/summitApp/Model.js
@@ -7,7 +7,36 @@ guidedModel =// @startlock
 		{// @endlock
 			submitSurveryAnswers:function(sessionSurveyArrayForSubmission)
 			{// @lock
-				console.log(sessionSurveyArrayForSubmission);
+				//console.log(sessionSurveyArrayForSubmission);
+				//var sessionID= sessionSurveyArrayForSubmission['sessionID'];
+				//var userCookieID = sessionSurveyArrayForSubmission['userCookieID'];
+				for (var obj in sessionSurveyArrayForSubmission) {
+//					if (obj == 'sessionID') { 
+//						sessionID = sessionSurveyArrayForSubmission[obj];
+//						break;
+//					}
+//					else if (obj == 'userCookieID') {
+//						userCookieID = sessionSurveyArrayForSubmission[obj];
+//						break;
+//					}
+//					else {
+//						
+//					}
+
+					if (obj != 'sessionID' & obj != 'userCookieID') { 
+						//console.log(obj + '   ' +sessionSurveyArrayForSubmission[obj]);
+						var questionID = obj;
+						//var answer = sessionSurveyArrayForSubmission[obj];
+						var newAnswer = ds.Answer.createEntity();
+						newAnswer.question = ds.Question.find('ID = :1',questionID);
+						newAnswer.sessionSurvey = newAnswer.question.sessionSurvey;
+						newAnswer.session = ds.Session.find('ID = :1',sessionSurveyArrayForSubmission['sessionID']);
+						newAnswer.userCookieID = sessionSurveyArrayForSubmission['userCookieID'];
+						newAnswer.question.isRating?newAnswer.rate = sessionSurveyArrayForSubmission[obj]:newAnswer.answer = sessionSurveyArrayForSubmission[obj];
+						newAnswer.save();
+					}
+				}
+				console.log(newAnswer.userCookieID);
 			}// @startlock
 		}
 	},
